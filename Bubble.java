d6892de0a4a80da554db8ca35a058d761764b9df
TheHarshu <106153918+TheHarshu@users.noreply.github.com>
2023-06-30 07:19:29 +0530

    Bubble.java
    This code allows you to initialise an array in ascending order by "Bubble Sorting Method".

--- Bubble.java
+++ Bubble.java
@@ -1,17 +1,20 @@
-import java.util.*;
+import java.util.*; /*Importing the scanner package */
 public class Bubble_Sort
 {
     public static void main(String[]args)
     {
-        Scanner sc = new Scanner(System.in);
-        int n[] = new int[10];
-        int t=0;
-        for (int i=0; i<10;i++)
+        Scanner sc = new Scanner(System.in); /*Creating the scanner object*/
+        int n[] = new int[10]; /*Defining array(You can change the number of elements*/
+        int t=0; /*Initialising a variable for passing the value*/
+        
+        for (int i=0; i<10;i++) /*Data entry loop*/
         {
             System.out.println("Enter number: ");
             n[i]=sc.nextInt();
             
         }
+        
+        /*Bubble sort process*/
         for (int i=0; i<9;i++)
         {
             for (int j=0;j<9-i;j++)
@@ -24,6 +27,8 @@ public class Bubble_Sort
                 }
             }
         }
+        
+        /*Printing the sorted array in ascending order*/
         System.out.println("The sorted array in ascending order is :");
           for (int i=0; i<10;i++)
           
